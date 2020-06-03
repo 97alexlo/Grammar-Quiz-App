ad816896be1fa796d77de5101f027ac24752d768
Alexander Lo <7253alex@gmail.com>
2020-06-02 21:51:41 -0700

    Updates

---
+++ precache-manifest.fdbe0fe6711bbf4dea90d1cde057a8a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ff30b51448656e087590e6c7876df8",
+    "url": "/react-grammar-app/index.html"
+  },
+  {
+    "revision": "5e1235011880184b542b",
+    "url": "/react-grammar-app/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "5119c8e51a906f558d80",
+    "url": "/react-grammar-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e1235011880184b542b",
+    "url": "/react-grammar-app/static/js/2.e560dc0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-grammar-app/static/js/2.e560dc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5119c8e51a906f558d80",
+    "url": "/react-grammar-app/static/js/main.ba23494b.chunk.js"
+  },
+  {
+    "revision": "36877e254008978423ef",
+    "url": "/react-grammar-app/static/js/runtime-main.205fd5d3.js"
+  }
+]);
